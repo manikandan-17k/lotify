a19b074be6e7b60897d6c2938c4f403a4d978dc3
MANIKANDAN-17K <mass00017@gmail.com>
2026-05-12 11:53:04 +0530

    reset

--- backend/src/route/albums.route.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { getAllAlbums, getAlbumById } from "../controller/album.controller.js"; 
-const router = Router();
-
-router.get("/", getAllAlbums);
-router.get("/:id", getAlbumById);
-export default router;
